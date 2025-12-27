6dc5390666bb9bd898cfa1d9105ae4c01675e80c
Jecricho Knox <hjimmyk@163.com>
2025-12-27 23:56:20 +0800

    新增alloc.h和lampz.h

--- include/lammp/version.h
+++ include/lammp/version.h
@@ -45,4 +45,22 @@
 #define LAMMP_DATE "12-26"
 #define LAMMP_COPYRIGHT "Copyright (C) [2025] [HJimmyK/LAMINA]"
 
+#include <stdio.h>
+
+static inline int lammp_version() {
+    if (LAMMP_COMPILER_VERSION == 0) {
+        printf("Unknown compiler\n");
+    } else {
+        printf("Compiler: %s\n", LAMMP_COMPILER);
+        printf("Compiler version: %d\n", LAMMP_COMPILER_VERSION);
+    }
+    printf("LAMMP version: %s\n", LAMMP_VERSION);
+    printf("LAMMP alpha year: %s\n", LAMMP_ALPHA_YEAR);
+    printf("LAMMP date: %s\n", LAMMP_DATE);
+    printf("LAMMP copyright: %s\n", LAMMP_COPYRIGHT);
+    printf("======================================================\n");
+    printf("Hello, lammp!\n");
+    return 0;
+}
+
 #endif /* __LAMMP_VERSION_H__ */
