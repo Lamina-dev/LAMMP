85feb65c10f34b28f31bb890d96d6fd3439c205b
Jecricho Knox <hjimmyk@163.com>
2026-01-01 16:52:05 +0800

    更改lampz_t的别名逻辑，使其更安全；引入lamp_sz别名，专门作为索引和长度变量类型

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Lamina的任意精度计算库。
 
 ## 基本类型
 
-- 整数：有符号整数 ``lampz_t``为结构体指针，该结构体指针内部存储大整数数组的头尾指针。数组默认采用64位整数。存储整数为其绝对值，负数通过使用负数的数组长度表示。
+- 整数：有符号整数 ``lampz_t``为 ``__struct_lampz``一维数组的别名，该结构体指针内部存储大整数数组的头尾指针。数组默认采用64位整数。存储整数为其绝对值，负数通过使用负数的数组长度表示。
 
 计算函数可以见于``lampz.h``中，该文件提供 C 接口。
 
 ## 编译
 
-目前Lammp仅支持Windows平台，采用mingw编译器，需要CMake编译。
+目前Lammp仅支持Windows平台，采用mingw编译器，使用CMake编译，目前暂不支持MSVC编译器，后续将支持。
 
 ## 快速开始
 
@@ -22,25 +22,23 @@ Lamina的任意精度计算库。
 #include <string>
 #include <algorithm>
 #include "include/lammp/lampz.h"
-
+#include "include/test/Test.hpp"
 
 int main() {
-    lampz_t z = nullptr;
-    std::string str = "0123781792787672657497878419374891734891374913874";
+    lampz_t z;
+    __lampz_init(z);
+    std::string str = "01237817927876726574923467362786482368238090123768293486732874";
     std::string str_copy(str.size(), '\0');
     std::copy(str.begin(), str.end(), str_copy.begin());
     std::reverse(str_copy.begin(), str_copy.end());
     std::cout << "computer " << str_copy << " + " << str_copy << " = " << std::endl;
 
     lampz_set_str(z, str.data(), str.size(), 10);
-    if (z == nullptr) {
-        std::cout << "Error: memory allocation failed" << std::endl;
-        return 1;
-    }
+    
 
-    lampz_add_x(z, z);
+    lampz_mul_x(z, z);
     std::string str3(lampz_to_str_len(z, 10), '\0');
-    lamp_ui str3_len = lampz_to_str(str3.data(), str3.capacity(), z, 10);
+    lamp_sz str3_len = lampz_to_str(str3.data(), str3.capacity(), z, 10);
     for (size_t i = str3_len; i-- != 0; ) {
         std::cout << str3[i];
     }
--- src/lammp/lampz/add_sub.cpp
+++ src/lammp/lampz/add_sub.cpp
@@ -9,20 +9,19 @@
 #include "../../../include/lammp/lampz.h"
 #include "../../../include/lammp/lammp.hpp"
 
-void lampz_add_xy(lampz_t& z, const lampz_t x, const lampz_t y) {
+void lampz_add_xy(lampz_t z, const lampz_t x, const lampz_t y) {
     bool x_sign = x->len > 0;
     bool y_sign = y->len > 0;
-    lamp_ui len_x = lampz_get_len(x);
-    lamp_ui len_y = lampz_get_len(y);
+    lamp_sz len_x = lampz_get_len(x);
+    lamp_sz len_y = lampz_get_len(y);
     if (len_x == 0 || len_y == 0) {
         lampz_free(z);
         return;
     }
-    lamp_ui z_cap = __lampz_get_capacity(z);
-    lamp_ui z_len = lammp::Arithmetic::get_add_len(len_x, len_y);
+    lamp_sz z_cap = __lampz_get_capacity(z);
+    lamp_sz z_len = lammp::Arithmetic::get_add_len(len_x, len_y);
     if (z_cap < z_len) {
-        lampz_free(z);
-        z = __lampz_talloc(z_len);
+        __lampz_talloc(z, z_len);
     }
     if (x_sign == y_sign) {
         lammp::Arithmetic::abs_add_binary(x->begin, len_x, y->begin, len_y, z->begin);
@@ -36,20 +35,19 @@ void lampz_add_xy(lampz_t& z, const lampz_t x, const lampz_t y) {
     return;
 }
 
-void lampz_sub_xy(lampz_t& z, const lampz_t x, const lampz_t y) {
+void lampz_sub_xy(lampz_t z, const lampz_t x, const lampz_t y) {
     bool x_sign = x->len > 0;
     bool y_sign = y->len > 0;
-    lamp_ui len_x = lampz_get_len(x);
-    lamp_ui len_y = lampz_get_len(y);
+    lamp_sz len_x = lampz_get_len(x);
+    lamp_sz len_y = lampz_get_len(y);
     if (len_x == 0 || len_y == 0) {
         lampz_free(z);
         return;
     }
-    lamp_ui z_cap = __lampz_get_capacity(z);
-    lamp_ui z_len = lammp::Arithmetic::get_sub_len(len_x, len_y);
+    lamp_sz z_cap = __lampz_get_capacity(z);
+    lamp_sz z_len = lammp::Arithmetic::get_sub_len(len_x, len_y);
     if (z_cap < z_len) {
-        lampz_free(z);
-        z = __lampz_talloc(z_len);
+        __lampz_talloc(z, z_len);
     }
     if (x_sign == y_sign) {
         lammp::Arithmetic::abs_sub_binary(x->begin, len_x, y->begin, len_y, z->begin);
@@ -63,17 +61,20 @@ void lampz_sub_xy(lampz_t& z, const lampz_t x, const lampz_t y) {
     return;
 }
 
-void lampz_add_x(lampz_t& z, const lampz_t x) {
+void lampz_add_x(lampz_t z, const lampz_t x) {
     bool x_sign = x->len > 0;
     bool z_sign = z->len > 0;
-    lamp_ui len_x = lampz_get_len(x);
-    lamp_ui len_z = lampz_get_len(z);
+    lamp_sz len_x = lampz_get_len(x);
+    lamp_sz len_z = lampz_get_len(z);
     if (len_x == 0 || len_z == 0) {
         lampz_free(z);
         return;
     }
-    lamp_ui z_len = lammp::Arithmetic::get_add_len(len_x, len_z);
-    __lampz_calloc(z, z_len);
+    lamp_sz z_len = lammp::Arithmetic::get_add_len(len_x, len_z);
+    lamp_sz z_cap = __lampz_get_capacity(z);
+    if (z_cap < z_len) {
+        __lampz_talloc(z, z_len);
+    }
     if (x_sign == z_sign) {
         lammp::Arithmetic::abs_add_binary(x->begin, len_x, z->begin, len_z, z->begin);
         z->len = lammp::Arithmetic::rlz(z->begin, z_len);
@@ -86,17 +87,20 @@ void lampz_add_x(lampz_t& z, const lampz_t x) {
     return;
 }
 
-void lampz_sub_x(lampz_t& z, const lampz_t x) {
+void lampz_sub_x(lampz_t z, const lampz_t x) {
     bool x_sign = x->len > 0;
     bool z_sign = z->len > 0;
-    lamp_ui len_x = lampz_get_len(x);
-    lamp_ui len_z = lampz_get_len(z);
+    lamp_sz len_x = lampz_get_len(x);
+    lamp_sz len_z = lampz_get_len(z);
     if (len_x == 0 || len_z == 0) {
         lampz_free(z);
         return;
     }
-    lamp_ui z_len = lammp::Arithmetic::get_sub_len(len_x, len_z);
-    __lampz_calloc(z, z_len);
+    lamp_sz z_len = lammp::Arithmetic::get_sub_len(len_x, len_z);
+    lamp_sz z_cap = __lampz_get_capacity(z);
+    if (z_cap < z_len) {
+        __lampz_talloc(z, z_len);
+    }
     if (x_sign == z_sign) {
         lammp::Arithmetic::abs_sub_binary(x->begin, len_x, z->begin, len_z, z->begin);
         z->len = lammp::Arithmetic::rlz(z->begin, z_len);
