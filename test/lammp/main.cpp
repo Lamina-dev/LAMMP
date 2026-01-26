c6e8a13340156d7737a4bf2614fb74e0d0f8bbc3
Jericho Knox <hjimmyk@163.com>
2026-01-26 09:23:52 +0800

    更改项目README，和main.c，将version正式更改至3.0.0

--- test/lammp/main.cpp
+++ test/lammp/main.cpp
@@ -4,44 +4,6 @@
 #include <vector>
 
 int main() {
-    // pri_int tab;
-    // uint n = 56000;
-    // lmmp_prime_int_init_(&tab, n);
-    
-    // int tsu = test_pow(2, 55993);
-    // if (tsu == 0) {
-    //     std::cout << "passed";
-    // } else {
-    //     std::cout << "failed";
-    // }
-    // std::cout << std::endl;
-
-    // std::vector<mp_limb_t> a_vec;
-    // std::vector<mp_limb_t> e_vec;
-    // a_vec.reserve(tab.prin);
-    // e_vec.reserve(tab.prin);
-    // for (int i = 0; i < tab.prin; ++i) {
-    //     mp_limb_t a = tab.pri[i];
-        
-    //     ulong pn = n;
-    //     mp_limb_t e = 0;
-    //     while (pn > 0) {
-    //         pn /= a;
-    //         e += pn;
-    //     }
-    //     if (e < 50) continue;
-        
-    //     e_vec.push_back(e);
-    //     a_vec.push_back(a);
-    // }
-
-    // for (int i = 0; i < a_vec.size(); ++i) {
-    //     if (test_pow(a_vec[i], e_vec[i]) == 0) {
-    //         std::cout << "passed" << std::endl;
-    //     } else {
-    //         std::cout << "failed" << std::endl;
-    //     }
-    // }
-    // test_pow(151, 1439);
+    test_umul64to128();
     return 0;
 }
