1ee5ce08b52deb343c1be4503be248966d3f9c7c
Jecricho Knox <hjimmyk@163.com>
2026-02-11 09:20:40 +0800

    更新测试

--- benchmark/lammp/src/bench_hash.cpp
+++ benchmark/lammp/src/bench_hash.cpp
@@ -6,15 +6,17 @@ long long bench_hash(int len, int type) {
     mp_ptr a = (mp_ptr)lmmp_alloc(len * sizeof(mp_limb_t));
     ger_random_numbers(len, a);
     if (type == 0) {
+        key128_t key = {1012321, 233320};
         auto start = std::chrono::high_resolution_clock::now();
-        uint64_t h = lmmp_siphash24_(a, len, (srckey128_t){1012321, 233320});
+        uint64_t h = lmmp_siphash24_(a, len, key);
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         lmmp_free(a);
         return duration.count();
     } else {
+        key64_t key = {1012321};
         auto start = std::chrono::high_resolution_clock::now();
-        uint64_t h = lmmp_xxhash_(a, len, 1012321);
+        uint64_t h = lmmp_xxhash_(a, len, key);
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         lmmp_free(a);
--- test/lammp/src/test_gcd.cpp
+++ test/lammp/src/test_gcd.cpp
@@ -1,11 +1,43 @@
 #include "../include/test_short.hpp"
 
 void test_gcd() {
-    // 示例：计算GCD(0x1234567890ABCDEF1234567890ABCDEF, 0x0FEDCBA0987654321FEDCBA0987654321)
-    // 两个数各2个limb（64位*2=128位）
-    mp_size_t rn = lmmp_nCr_size_(10000, 600);
-    mp_ptr dst = ALLOC_TYPE(rn - 4, mp_limb_t);
-    rn = lmmp_nCr_(dst, rn, 10000, 600);
+    TEMP_DECL;
 
-    lmmp_free(dst);
+    mp_size_t an = 10, bn = 10, cn = 10;
+
+    mp_ptr a = TALLOC_TYPE(an, mp_limb_t);
+    mp_ptr b = TALLOC_TYPE(bn, mp_limb_t);
+    mp_ptr _a = TALLOC_TYPE(an + cn, mp_limb_t);
+    mp_ptr _b = TALLOC_TYPE(bn + cn, mp_limb_t);
+    mp_ptr c = TALLOC_TYPE(cn, mp_limb_t);
+
+    lmmp_global_rng_init_(20, 0);
+    lmmp_random_(a, an);
+    lmmp_random_(b, bn);    
+    lmmp_random_(c, cn);
+
+    lmmp_mul_(_a, a, an, c, cn);
+    an += cn;
+    an -= _a[an - 1] == 0 ? 1 : 0;
+
+    lmmp_mul_(_b, b, bn, c, cn);
+    bn += cn;
+    bn -= _b[bn - 1] == 0 ? 1 : 0;
+
+    mp_size_t rn = LMMP_MIN(an, bn);
+    mp_ptr dst = TALLOC_TYPE(rn, mp_limb_t);
+    rn = lmmp_gcd_basecase_(dst, _a, an, _b, bn);
+
+    for (mp_size_t i = 0; i < rn; i++) {
+        std::cout << dst[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    for (mp_size_t i = 0; i < cn; i++) {
+        std::cout << c[i] << " ";
+    }
+    std::cout << std::endl;
+
+    TEMP_FREE;
 }
