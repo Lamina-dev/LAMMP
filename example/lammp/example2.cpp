2a0aad9bfc1147f15cf8cc82597b24566d162991
Jecricho Knox <hjimmyk@163.com>
2026-02-28 22:33:41 +0800

    更新example

--- example/lammp/example2.cpp
+++ example/lammp/example2.cpp
@@ -3,11 +3,12 @@
 #include "../../include/lammp/numth.h"
 
 int main() {
-    uint n = 10000000;
+    uint n = 100000;
     printf("calculating factorial of %d...\n", n);
     mp_size_t len = lmmp_factorial_size_(n);
     mp_ptr dst = (mp_ptr)lmmp_alloc(len * sizeof(mp_limb_t));
     len = lmmp_factorial_(dst, len, n);
     printf("completed.\n");
+    lmmp_free(dst);
     return 0;
 }
