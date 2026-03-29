83bb22b46f3d791a3cca677a1cf3b0bed10792e3
Jecricho Knox <hjimmyk@163.com>
2026-03-29 14:30:32 +0800

    新增toom44，toom43，toom52等乘法；新增部分插值算法；其他改动

--- src/lammp/mul_toom32.c
+++ src/lammp/mul_toom32.c
@@ -7,7 +7,6 @@
 #include "../../include/lammp/lmmpn.h"
 
 /*
-
 Evaluate in: -1, 0, +1, +inf
 
 <-s-><--n--><--n-->
@@ -19,18 +18,17 @@ v0  =  a0       * b0     #   A(0)*B(0)
 v1  = (a0+a1+a2)*(b0+b1) #   A(1)*B(1)      ah  <= 2  bh <= 1
 vm1 = (a0-a1+a2)*(b0-b1) #  A(-1)*B(-1)    |ah| <= 1  bh = 0
 vinf=        a2 *    b1  # A(inf)*B(inf)
-
 */
 
-void lmmp_mul_toom32_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_srcptr numb, mp_size_t nb) {
+void lmmp_mul_toom32_(mp_ptr restrict dst, mp_srcptr restrict numa, mp_size_t na, mp_srcptr restrict numb, mp_size_t nb) {
     lmmp_param_assert(nb >= 12);
     lmmp_param_assert(4 * na >= 5 * nb);
     lmmp_param_assert(5 * na <= 9 * nb);
-    TEMP_DECL;
+    TEMP_S_DECL;
     mp_size_t n = 1 + (2 * na >= 3 * nb ? (na - 1) / 3 : (nb - 1) >> 1), s = na - 2 * n, t = nb - n;
     int vm1_neg;
     mp_limb_t cy, hi;
-    mp_limb_t* tp = SALLOC_TYPE(4 * n + 2, mp_limb_t);
+    mp_limb_t* restrict tp = SALLOC_TYPE(4 * n + 2, mp_limb_t);
 
 #define a0 numa
 #define a1 (numa + n)
@@ -118,5 +116,5 @@ void lmmp_mul_toom32_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_srcptr numb, 
     cy = vm1[2 * n] + lmmp_add_(r1, vm1, 2 * n, r1, n);
     lmmp_add_(r2, r2, n + s + t, v1, 2 * n + 1);
     lmmp_inc_1(r3, cy);
-    TEMP_FREE;
+    TEMP_S_FREE;
 }
