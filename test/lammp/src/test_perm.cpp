70f44233b3716e76827aa5d63182c2669b2eb0d4
Jecricho Knox <hjimmyk@163.com>
2026-04-10 19:18:21 +0800

    更新test

--- test/lammp/src/test_perm.cpp
+++ test/lammp/src/test_perm.cpp
@@ -5,50 +5,50 @@
  */
 
 #include <chrono>
+
 #include "../include/test_short.hpp"
+
 
 #define ALLOC_TYPE(n, type) (type*)lmmp_alloc((n) * sizeof(type))
 
 void test_perm() {
     lmmp_stack_init();
-    const size_t n = 0x2ffffff, r = n / 13.5;
+    const size_t n = 0x12203, r = 52222;
     size_t len = lmmp_nPr_size_(n, r);
     std::cout << "len = " << len << std::endl;
 
     mp_ptr a;
     mp_ptr b = ALLOC_TYPE(len, mp_limb_t);
 
-    mp_ptr limb_vec = ALLOC_TYPE(n, mp_limb_t);
-    for (size_t i = 0; i < n - r; i++) {
-        limb_vec[i] = 1;
-    }
-    for (size_t i = n - r; i < n; i++) {
-        limb_vec[i] = i + 1;
+    mp_limb_t* limb_vec = ALLOC_TYPE(r, mp_limb_t);
+    for (size_t i = 1; i <= r; i++) {
+        limb_vec[i - 1] = n - r + i;
     }
 
     auto start = std::chrono::high_resolution_clock::now();
-    mp_size_t an = lmmp_limb_elem_mul_(&a, limb_vec, n);
+    mp_size_t an = lmmp_limb_elem_mul_(&a, limb_vec, r);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-    std::cout << "Time elapsed: (native)" << duration << " microseconds" << std::endl;
+    std::cout << "Time elapsed: " << duration << " microseconds" << std::endl;
 
     auto start2 = std::chrono::high_resolution_clock::now();
     mp_size_t bn = lmmp_nPr_(b, len, n, r);
     auto end2 = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count();
-    std::cout << "Time elapsed: (queued)" << duration2 << " microseconds" << std::endl;
+    std::cout << "Time elapsed: " << duration2 << " microseconds" << std::endl;
 
     if (bn != an)
         std::cout << "bn != an\n" << bn << " != " << an << std::endl;
-    else 
+    else
         std::cout << bn << " == " << an << std::endl;
     for (size_t i = 0; i < an; i++) {
         if (a[i] != b[i]) {
             std::cout << "i = " << i << " a[i] = " << a[i] << " b[i] = " << b[i] << std::endl;
-            break;
+            goto fail;
         }
     }
     std::cout << "passed\n";
+fail:
     lmmp_free(a);
     lmmp_free(b);
     lmmp_free(limb_vec);
