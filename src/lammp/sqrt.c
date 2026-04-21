322f80dfac1dd3ee4ef6401b6bbb938d8d6b1e48
Jecricho Knox <hjimmyk@163.com>
2026-04-21 14:54:37 +0800

    修复错误的头文件包含，和编译错误

--- src/lammp/sqrt.c
+++ src/lammp/sqrt.c
@@ -4,8 +4,9 @@
  * See LICENSE in the project root for the full license text.
  */
 
-#include "../../../include/lammp/impl/mpdef.h"
-#include "../../../include/lammp/impl/tmp_alloc.h"
+#include "../../include/lammp/impl/mparam.h"
+#include "../../include/lammp/impl/mpdef.h"
+#include "../../include/lammp/impl/tmp_alloc.h"
 #include "../../include/lammp/lmmpn.h"
 
 // round(sqrt(2^25/(i+128+1/2)))-256
