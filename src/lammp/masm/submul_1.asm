f0aac556ffc66d3ca735e5d16c5222e55f177c11
Jecricho Knox <hjimmyk@163.com>
2026-05-07 21:15:22 +0800

    删除masm文件

--- src/lammp/masm/submul_1.asm
+++
@@ -1,8 +0,0 @@
- ; LAMMP - Copyright (C) 2025-2026 HJimmyK(Jericho Knox)
- ; This file is part of lammp, under the GNU LGPL v2 license.
- ; See LICENSE in the project root for the full license text.
- 
-include <asm_windows>
-func_1 TEXTEQU <lmmp_submul_1_>
-ADDSUB TEXTEQU <sub>
-include <aorsmul_1>
