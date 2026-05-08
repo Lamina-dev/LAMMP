30c23d283ccbfeb3966a9d2342ec5d4108fa9d14
Jericho Knox <hjimmyk@163.com>
2026-05-08 22:45:49 +0800

    新增128bit，256bit逆元计算

--- src/lammp/NumTh/binvert_1.c
+++ src/lammp/NumTh/binvert_1.c
@@ -5,6 +5,8 @@
  */
 
 #include "../../../include/lammp/numth.h"
+#include "../../../include/lammp/lmmpn.h"
+#include "../../../include/lammp/impl/longlong.h"
 
 /* from https://arxiv.org/abs/2204.04342 */
 
@@ -17,6 +19,7 @@ static const unsigned char binv_tab[128] = {
     117, 31,  33,  203, 237, 215, 89,  195, 229, 15,  17,  59,  93,  199, 73,  51,  85,  255};
 
 uint lmmp_binvert_uint_(uint a) {
+    lmmp_param_assert(a % 2 == 1);
     ulong r, y;
 
     r = binv_tab[(a / 2) & 0x7F]; /* 8 bits */
@@ -28,6 +31,7 @@ uint lmmp_binvert_uint_(uint a) {
 }
 
 ulong lmmp_binvert_ulong_(ulong a) {
+    lmmp_param_assert(a % 2 == 1);
     ulong r, y;
 
     r = binv_tab[(a / 2) & 0x7F]; /* 8 bits */
@@ -38,4 +42,64 @@ ulong lmmp_binvert_ulong_(ulong a) {
     y *= y;
     r = r * (1 + y); /* 64 bits */
     return r;
+}
+
+void lmmp_binvert_2_(mp_ptr dst, mp_srcptr numa) {
+    lmmp_param_assert(numa[0] % 2 == 1);
+    mp_limb_t k, t;
+    mp_limb_t a1 = numa[1];
+    mp_limb_t a0 = numa[0];
+    mp_limb_t xn = lmmp_binvert_ulong_(a0);
+    mp_limb_t yn[2] = {xn, 0};
+    mp_limb_t z[2];
+    /*
+     xn * a0 == 1 + k * B
+     yn := xn * (2 - a * xn) mod B^2
+        := xn * (2 - a0 * xn - a1 * xn * B) mod B^2
+        := xn * (2 - 1 - k*B - a1 * xn * B) mod B^2
+        := xn * (1 - k*B - a1 * xn * B) mod B^2
+        := (xn - xn*k * B - a1 * xn^2 * B) mod B^2
+    */
+    _umul64to128_(a0, xn, &t, &k);
+    z[0] = 0;
+    z[1] = xn * k;
+    z[1] += a1 * xn * xn;
+    _u128sub(yn, yn, z);
+    dst[0] = yn[0];
+    dst[1] = yn[1];
+}
+
+void lmmp_binvert_4_(mp_ptr restrict dst, mp_srcptr restrict numa) {
+    /*
+           a == a0 + a1 * B^2
+     xn * a0 == 1 + k * B^2
+     yn := xn * (2 - a * xn) mod B^4
+        := xn * (2 - a0 * xn - a1 * xn * B^2) mod B^4
+        := xn * (2 - 1 - k*B^2 - a1 * xn * B^2) mod B^4
+        := xn * (1 - k*B^2 - a1 * xn * B^2) mod B^4
+        := (xn - xn*k * B^2 - a1 * xn^2 * B^2) mod B^4
+    */
+    lmmp_binvert_2_(dst, numa);
+    mp_limb_t k[4];
+    mp_limb_t z[4];
+    mp_limb_t t[2];
+    _umul128to256_(dst[1], dst[0], numa[1], numa[0], k);
+    lmmp_debug_assert(k[1] == 0 && k[0] == 1);
+    dst[2] = 0;
+    dst[3] = 0;
+#define xn (dst)
+#define k (k + 2)
+    z[0] = 0;
+    z[1] = 0;
+    _umul128to128_(k[1], k[0], xn[1], xn[0], z + 2);
+    
+    _umul64to128_(xn[0], xn[0], t, t + 1);
+    t[1] += (xn[1] * xn[0]) << 1;
+    _umul128to128_(t[1], t[0], numa[3], numa[2], t);
+
+    _u128add(z + 2, z + 2, t);
+    lmmp_sub_n_(dst, dst, z, 4);
+
+#undef xn
+#undef k
 }
