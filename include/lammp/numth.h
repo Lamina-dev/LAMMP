dc4b7ca7340e13fee7d6bdd524781e0630340693
Jericho Knox <hjimmyk@163.com>
2026-05-08 22:46:45 +0800

    新增binvert函数声明

--- include/lammp/numth.h
+++ include/lammp/numth.h
@@ -52,6 +52,7 @@ typedef int64_t* slongp;
 /**
  * @brief 计算 a 在2^32下的逆元
  * @param a 待求逆元
+ * @param a%2==1
  * @return 逆元
  */
 uint lmmp_binvert_uint_(uint a);
@@ -59,9 +60,37 @@ uint lmmp_binvert_uint_(uint a);
 /**
  * @brief 计算 a 在2^64下的逆元
  * @param a 待求逆元
+ * @param a%2==1
  * @return 逆元
  */
 ulong lmmp_binvert_ulong_(ulong a);
+
+/**
+ * @brief 计算 [numa,2] 在B^2下的逆元
+ * @param numa 待求逆元指针（长度为 2 个limb）
+ * @param dst 结果指针（长度为 2 个limb）
+ * @warning numa!=NULL, dst!=NULL, numa[0]%2==1, eqsep(dst,numa)
+ */
+void lmmp_binvert_2_(mp_ptr dst, mp_srcptr numa);
+
+/**
+ * @brief 计算 [numa,4] 在B^4下的逆元
+ * @param numa 待求逆元指针（长度为 4 个limb）
+ * @param dst 结果指针（长度为 4 个limb）
+ * @warning numa!=NULL, dst!=NULL, numa[0]%2==1, sep(dst,numa)
+ */
+void lmmp_binvert_4_(mp_ptr dst, mp_srcptr numa);
+
+/**
+ * @brief 计算 [numa,na] 在B^n 下的逆元
+ * @param dst 结果指针（长度为 n 个limb）
+ * @param numa 待求逆元指针（长度为 na 个limb）
+ * @param na 待求逆元的 limb 长度
+ * @param n 结果的 limb 长度
+ * @warning n>=na>0, numa!=NULL, dst!=NULL, numa[0]%2==1, sep(dst,numa)
+ * @return dst 的实际 limb 长度
+ */
+mp_size_t lmmp_binvert_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_size_t n);
 
 /**
  * @brief 计算两个无符号整数的最大公约数
