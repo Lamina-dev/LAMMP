a8ff72ab78378a068426b496124aac48e9ec5517
Jericho Knox <hjimmyk@163.com>
2026-06-25 11:41:23 +0800

    删除已移除的函数；新增test_divexact

--- test/lammp/src/test_divexact.cpp
+++ test/lammp/src/test_divexact.cpp
@@ -105,3 +105,52 @@ void test_divexact_basecase() {
     lmmp_free(p2);
     lmmp_free(p3);
 }
+
+void test_divexact() {
+    mp_size_t bn = 17131;
+    mp_size_t n = 16220;
+    mp_ptr b = ALLOC_TYPE(bn, mp_limb_t);
+    lmmp_seed_random_(b, bn, 19123311, 1);
+    b[0] |= 1;
+
+    mp_ptr p0 = (mp_ptr)lmmp_alloc(n * sizeof(mp_limb_t));
+    lmmp_seed_random_(p0, n, 198132271, 2);
+    mp_ptr p1 = (mp_ptr)lmmp_alloc((n + bn) * sizeof(mp_limb_t));
+
+    if (bn >= n)
+        lmmp_mul_(p1, b, bn, p0, n);
+    else
+        lmmp_mul_(p1, p0, n, b, bn);
+
+    mp_size_t p1n = n + bn;
+    p1n -= p1[p1n - 1] == 0;
+
+    mp_ptr p2 = (mp_ptr)lmmp_alloc((p1n - bn + 1) * sizeof(mp_limb_t));
+    auto start = std::chrono::high_resolution_clock::now();
+    lmmp_divexact_divide_(p2, p1, p1n, b, bn);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " us"
+              << std::endl;
+
+    mp_ptr p3 = (mp_ptr)lmmp_alloc((p1n - bn + 1) * sizeof(mp_limb_t));
+    start = std::chrono::high_resolution_clock::now();
+    lmmp_divexact_(p3, p1, p1n, b, bn);
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " us"
+              << std::endl;
+
+    for (mp_size_t i = 0; i < p1n - bn; i++) {
+        if (p3[i] != p2[i] || p0[i] != p3[i]) {
+            printf("Error at index %llu\n", i);
+            break;
+        }
+    }
+    if (p3[p1n - bn] != 0 || p2[p1n - bn] != 0) {
+        printf("Error at index %llu\n", p1n - bn);
+    } 
+    lmmp_free(b);
+    lmmp_free(p0);
+    lmmp_free(p1);
+    lmmp_free(p2);
+    lmmp_free(p3);
+}
