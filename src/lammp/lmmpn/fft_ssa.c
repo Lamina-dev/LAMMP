a947f469844b9ef4365d4fc38080a5aa65ab3ec6
Jericho Knox <hjimmyk@163.com>
2026-08-09 13:08:40 +0800

    fix assert

--- src/lammp/lmmpn/fft_ssa.c
+++ src/lammp/lmmpn/fft_ssa.c
@@ -82,9 +82,9 @@ mp_size_t lmmp_fft_next_size_(mp_size_t n) {
 void* lmmp_fft_memstack_(fft_memstack* ms, mp_size_t size) {
     if (size) {
         if (++ms->tempdepth > ms->maxdepth) {
+            lmmp_debug_assert(ms->maxdepth < FFT_MEMSTACK_DEPTH);
             ms->mem[++ms->maxdepth] = lmmp_alloc(size);
             ms->memsize[ms->maxdepth] = size;
-            lmmp_assert(ms->maxdepth < FFT_MEMSTACK_DEPTH);
         }
         lmmp_debug_assert(ms->memsize[ms->tempdepth] == size);
         return ms->mem[ms->tempdepth];
