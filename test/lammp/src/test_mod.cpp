bba26555c57608a508a6329110d17879630c5f52
Jericho Knox <jerichoknox@JerichodeMacBook-Air.local>
2026-08-19 20:20:20 +0800

    fix errors on macOS

--- test/lammp/src/test_mod.cpp
+++ test/lammp/src/test_mod.cpp
@@ -19,7 +19,7 @@
 
 void test_mod() {
     mp_size_t n = 1000000;
-    std::vector<mp_limb_t[4]> a(n);
+    std::vector<std::array<mp_limb_t, 4>> a(n);
     for (mp_size_t i = 0; i < n; i++) {
         a[i][0] = (i + 1) * rand();
         a[i][1] = (i + 2) * rand();
@@ -31,7 +31,7 @@ void test_mod() {
     auto start = std::chrono::high_resolution_clock::now();
     for (mp_size_t i = 0; i < n; i++) {
         mp_limb_t t[2] = {m[0], m[1]};
-        lmmp_div_2_(a[i], a[i], 4, t);
+        lmmp_div_2_(a[i].data(), a[i].data(), 4, t);
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto d = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
